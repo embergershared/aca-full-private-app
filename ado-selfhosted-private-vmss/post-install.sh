a7106ff4498b06533090809f7996090839b8a717
Emmanuel <bergerat62@gmail.com>
2025-06-20 15:40:49 -0400

    Added docker to VMSS ADO agent post-install

--- ado-selfhosted-private-vmss/post-install.sh
+++ ado-selfhosted-private-vmss/post-install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 az cli --version
 touch /2025-06-16-custom-script-is-here.txt
+
+# Install Docker for Ubuntu with convenient script
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh ./get-docker.sh --dry-run
